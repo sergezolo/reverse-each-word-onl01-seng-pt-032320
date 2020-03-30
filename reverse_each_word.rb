e8e1e8a3613ccb103428ec7543012e5035d72cc7
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-03-30 22:02:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(phrase)
   array = phrase.split(" ")
   new_array = []
-  array.each {|word| new_array << word.reverse}
-  return new_array.join(" ")
+  array.collect {|word| new_array << word.reverse}
+  new_array.join(" ")
 end
